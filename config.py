23b21b56a200c7b64048d575c04f09a2f394352f
justrightdecorators-ops <justrightdecorators@gmail.com>
2026-05-20 10:25:57 +0100

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-STRIPE_SECRET = os.environ.get("STRIPE_SECRET", "")
-BASE_URL = os.environ.get("BASE_URL", "https://sebbi.pro")
-PORT = int(os.environ.get("PORT", 8080))
-FREE_TIER_LIMIT = 1000
-VERSION = "2.0"
-PRODUCT = "AILeash"
