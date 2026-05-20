08f5041cd815ca7d472849ff071f46fc9b33f407
justrightdecorators-ops <justrightdecorators@gmail.com>
2026-05-20 21:34:26 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+STRIPE_SECRET = os.environ.get("STRIPE_KEY", "")
+BASE_URL = os.environ.get("BASE_URL", "https://sebbi.pro")
+PORT = int(os.environ.get("PORT", 8080))
+FREE_TIER_LIMIT = 1000
+VERSION = "2.0"
+PRODUCT = "AILeash"
