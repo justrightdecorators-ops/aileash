bc34b1899f6100793fe56d058c6b3efe3352c7b6
justrightdecorators-ops <justrightdecorators@gmail.com>
2026-06-17 23:59:03 +0100

    Delete features.py

--- features.py
+++
@@ -1,7 +0,0 @@
-from pyramid.view import view_config
-from pyramid.response import Response
-import lander
-
-@view_config(route_name='features_page', route_pattern='/features')
-def features_view(request):
-    return Response(lander.LANDER_HTML, content_type='text/html')
